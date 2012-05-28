613fde4b1d78146bb393c010aabf2cfa7c9e4fc8
Pip Taylor <pip@evilgeek.co.uk>
2012-05-28 10:32:34 +0100

    Working Roman numeral to decimal algorithm.

--- roman.js
+++ roman.js
@@ -1,3 +1,34 @@
-function toDecimal(numerals) {
+if (typeof(window.RomanNumerals) === "undefined") {
+  (function() {
+    window.RomanNumerals = {
+      values: {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+      },
 
+      toDecimal: function(numerals) {
+        var sum = 0;
+        var maxNumeral = 0;
+        for (var i = numerals.length - 1; i >= 0; i--) {
+          var value = RomanNumerals.valueFor(numerals.charAt(i));
+          if (value < maxNumeral) {
+            sum -= value;
+          } else {
+            sum += value;
+          }
+          maxNumeral = Math.max(value, maxNumeral);
+        }
+        return sum;
+      },
+
+      valueFor: function(numeral) {
+        return RomanNumerals.values[numeral.toUpperCase()] || 0;
+      }
+    }
+  })();
 }
